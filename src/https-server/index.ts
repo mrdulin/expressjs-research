5404a97b63dca592997a7c9c8994583a66d3b8a1
mrdulin <novaline.dulin@gmail.com>
2018-09-05 16:16:16 +0800

    add typescript and https-server sample

---
+++ src/https-server/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import https from 'https';
+import path from 'path';
+import fs from 'fs';
+
+import { logger } from '../utils';
+
+interface IOptions {
+  HTTP_PORT: number;
+  HTTPS_PORT: number;
+}
+
+function main(opts: IOptions) {
+  const app: express.Application = express();
+  const serverOptions: https.ServerOptions = {
+    key: fs.readFileSync(path.resolve(__dirname, '../../ssl/key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, '../../ssl/cert.pem'))
+  };
+  const httpServer: http.Server = http.createServer(app);
+  const httpsServer: https.Server = https.createServer(serverOptions, app);
+
+  app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('normal');
+  });
+
+  httpServer.listen(opts.HTTP_PORT, () =>
+    logger.info(`http server is listening on http://localhost:${opts.HTTP_PORT}`)
+  );
+  httpsServer.listen(opts.HTTPS_PORT, () => {
+    logger.info(`https server is listening on http://localhost:${opts.HTTPS_PORT}`);
+  });
+}
+
+main({ HTTP_PORT: 3000, HTTPS_PORT: 3001 });
---
+++ src/https-server/readme.md
@@ -0,0 +1,5 @@
+# https server and http server
+
+## 问题
+
+https://stackoverflow.com/questions/44145936/chrome-active-content-with-certificate-errors
