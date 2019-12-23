0d00b27e5241b4ddb713ab2767c2a44f3586c47e
mrdulin <novaline.dulin@gmail.com>
2019-12-23 14:56:48 +0800

    improve sample

--- src/stackoverflow/59450619/app.js
+++ src/stackoverflow/59450619/app.js
@@ -1,25 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 8080;
 const router = express.Router();
-const session = require("express-session");
+const session = require('express-session');
+const fs = require('fs');
+const path = require('path');
 
 app.use(
   session({
-    secret: "work hard",
+    secret: 'work hard',
     resave: true,
-    saveUninitialized: false
-  })
+    saveUninitialized: false,
+  }),
 );
 
-app.get("/", function(req, res, next) {
-  req.session.myname = "nathan";
+app.get('/', function(req, res, next) {
+  req.session.myname = 'nathan';
+  console.log(req.session.myname);
+  const htmlContent = fs.readFileSync(path.resolve(__dirname, './public/app.html')).toString();
+  res.send(htmlContent);
+});
+
+app.get('/me', function(req, res, next) {
   console.log(req.session.myname);
   res.sendStatus(200);
 });
 
-app.get("/me", function(req, res, next) {
-  console.log(req.session.myname);
+app.get('/ajax', function(req, res) {
+  console.log('req.session.myname: ', req.session.myname);
   res.sendStatus(200);
 });
 
