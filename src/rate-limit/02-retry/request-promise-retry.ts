76e7986a3ef714e1907cf1e8652d07cd63b9046a
mrdulin <novaline.dulin@gmail.com>
2020-02-18 18:05:17 +0800

    improve exponential backoff strategy

--- src/rate-limit/02-retry/request-promise-retry.ts
+++ src/rate-limit/02-retry/request-promise-retry.ts
@@ -4,6 +4,10 @@ async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function getNextRetryDelay(retryNumber) {
+  return Math.pow(2, retryNumber) * 1000 + Math.floor(Math.random() * 1000);
+}
+
 // https://developers.google.com/analytics/devguides/reporting/core/v3/coreErrors#backoff
 export async function requestWithExponentialBackoff(url) {
   for (let i = 0; i < 5; i++) {
@@ -12,7 +16,7 @@ export async function requestWithExponentialBackoff(url) {
     } catch (error) {
       console.log(`[${new Date().toLocaleTimeString()}] retry count ${i + 1}`);
       if (error.statusCode === 429) {
-        await sleep(Math.pow(i, 2) * Math.random() * 1000);
+        await sleep(getNextRetryDelay(i));
       } else {
         break;
       }
