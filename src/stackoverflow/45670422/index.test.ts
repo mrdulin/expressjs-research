3367f2f415ac534eb9879e53d1698ad966b2318f
mrdulin <novaline.dulin@gmail.com>
2020-10-09 15:51:34 +0800

    add example

---
+++ src/stackoverflow/45670422/index.test.ts
@@ -0,0 +1,16 @@
+import sinon from 'sinon'
+import { assert, expect } from 'chai';
+
+describe('45670422', () => {
+  it('should pass', () => {
+    const errorStub = sinon.stub(console, 'error').callsFake((msg) => {
+      console.log(`[[ERROR]] MSG: ${msg}`);
+      assert.fail(`actual`, `expected`, `message`);
+      throw new Error('we do not want to see this error');
+    }) 
+    console.error('network');
+  });
+  it('should equal 2', () => {
+    expect(1+1).to.be.eq(2)
+  });
+});
