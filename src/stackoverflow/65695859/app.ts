4986bb97513f677f9fa27f5af9e5128ae44394b1
mrdulin <novaline.dulin@gmail.com>
2021-01-13 13:50:53 +0800

    add example

---
+++ src/stackoverflow/65695859/app.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, Router } from 'express';
+import session from 'express-session';
+
+const app = express();
+const routes = Router();
+
+interface userInformation {
+  username: string;
+  email: string;
+}
+
+declare module 'express-session' {
+  interface SessionData {
+    user: userInformation;
+  }
+}
+class AppAPIController {
+  public getUserByAuth(req: Request, res: Response) {
+    return res.json(req.session.user);
+  }
+}
+
+const controller = new AppAPIController();
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  }),
+);
+routes.get('/@me', controller.getUserByAuth);
