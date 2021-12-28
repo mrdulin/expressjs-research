dc1a3ffdf2b7e7738cbeb67fe3538b98a33a24a1
mrdulin <novaline.dulin@gmail.com>
2021-12-28 14:14:34 +0800

    test: add extends express Request interface example

---
+++ src/stackoverflow/70486812/index.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+
+interface IUser {
+  name: string;
+}
+
+declare module 'express' {
+  interface Request {
+    user: IUser;
+  }
+}
+
+const controller = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.user.);
+};
