528074f965c9331e618d58666bdcb2c7d4b83d3f
official_dulin <novaline.dulin@gmail.com>
2022-11-28 03:21:04 +0000

    test: add example

---
+++ src/stackoverflow/74586526/index.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const { expect } = chai;
+
+describe("User login", First);
+
+function First() {
+  it("user should login", Second);
+}
+
+function Second() {
+  expect(1 + 1).to.be.eql(2);
+}
