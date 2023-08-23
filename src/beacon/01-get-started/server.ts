f6e2dfe4b930ea531f96c7bbe6491bd39d76ffe1
mrdulin <novaline.dulin@gmail.com>
2023-08-24 05:12:15 +0800

    feat: add beacon 01-get-started example

---
+++ src/beacon/01-get-started/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.post('/beacon/text', express.text(), (req, res) => {
+	console.log(req.url, 'req.body: ', req.body, typeof req.body);
+	res.sendStatus(204);
+});
+
+app.post('/beacon/blob', express.json(), (req, res) => {
+	console.log(req.url, 'req.body: ', req.body, typeof req.body);
+	res.sendStatus(204);
+});
+
+app.listen(3000, () => console.log('server is listening on port 3000'));
